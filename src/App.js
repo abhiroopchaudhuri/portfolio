e2896f442cd91fdc280ce210d763b65d9d315ccc
Abhiroop <abhiroopchaudhuri7@gma.com>
2024-06-17 21:31:54 +0530

    homepage fix 2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Footer from "./Footer";
 
 function App() {
   return (
-    <Router>
+    <Router basename="/portfolio">
       <div className="App bg-black flex flex-col min-h-screen">
         <Navbar />
 
