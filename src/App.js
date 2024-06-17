92afce76783b4b57709a08f365fdf108818f946c
Abhiroop <abhiroopchaudhuri7@gma.com>
2024-06-18 00:29:08 +0530

    hashrouter

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Footer from "./Footer";
 function App() {
   
   return (
-    <Router basename="/portfolio">
+    <Router>
       <div className="App bg-black flex flex-col min-h-screen">
         <Navbar />
 
