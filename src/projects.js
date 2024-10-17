166b0679dd256804ffa238b0a19488be58f471c1
Abhiroop <abhiroopchaudhuri7@gmail.com>
2024-10-18 00:36:33 +0530

    add project

--- src/projects.js
+++ src/projects.js
@@ -17,6 +17,14 @@ const Projects = [
     },
     {
         id: 2,
+        title: "Cineverse: Next-Gen Movie App",
+        description: `Features 🚀 <br/>-Dynamic Movie Sliders: Seamless browsing through popular, top-rated, and upcoming movies<br/>-Auto-playing Hero Section: Engaging visual showcase of featured films<br/>-Detailed Movie Pages: In-depth information, trailers, and cast details for each movie<br/>-Responsive Design: Optimized for all devices, from mobile to desktop<br/>-Real-time Data: Up-to-date movie information fetched from TMDb API<br/><br/>`,
+        githubLink: "https://github.com/abhicode7/cineverse-movie-app",
+        liveLink: "https://cineverse-movie-app.vercel.app/",
+        imageLink: "https://private-user-images.githubusercontent.com/87370756/376662935-c1466b5d-31d4-4d31-93d2-53dbf83dcad3.png?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3MjkxOTIwMTksIm5iZiI6MTcyOTE5MTcxOSwicGF0aCI6Ii84NzM3MDc1Ni8zNzY2NjI5MzUtYzE0NjZiNWQtMzFkNC00ZDMxLTkzZDItNTNkYmY4M2RjYWQzLnBuZz9YLUFtei1BbGdvcml0aG09QVdTNC1ITUFDLVNIQTI1NiZYLUFtei1DcmVkZW50aWFsPUFLSUFWQ09EWUxTQTUzUFFLNFpBJTJGMjAyNDEwMTclMkZ1cy1lYXN0LTElMkZzMyUyRmF3czRfcmVxdWVzdCZYLUFtei1EYXRlPTIwMjQxMDE3VDE5MDE1OVomWC1BbXotRXhwaXJlcz0zMDAmWC1BbXotU2lnbmF0dXJlPWQzMzRhZDExZGM2MzhmMjEyODIwZGRlMWNhOTEwODFhMWJiMzgwOTFiZjgyMDA0NTJkOTBkMTZlZGJmYjE0NDYmWC1BbXotU2lnbmVkSGVhZGVycz1ob3N0In0.RSqqHn4v3GhmqwsYfXLag3sgzbVvxKeOwgXRcl8RuZk"
+    },
+    {
+        id: 3,
         title: "Voyager - AI Image and Prompt Gallery",
         description: `Features 🌟<br/>
 -Responsive Masonry Layout: Enjoy a beautiful, Pinterest-style masonry layout that adapts perfectly to any 
@@ -29,22 +37,6 @@ screen size.<br/>
         liveLink: "https://abhicode7.github.io/voyager/",
         imageLink: "https://github.com/abhicode7/portfolio/assets/87370756/b4594a1d-547e-4518-9af6-f99e7f5727cf"
     },
-
-    {
-        id: 3,
-        title: "Holovista Metaverse Demo",
-        description: `Features 🛰️<br/>-Interactive Navigation: Seamlessly explore different sections of the website, including About, Explore, Insights, and more.<br/>
-
--Immersive Hero Section: Engage with visually stunning animations and captivating visuals in the Hero section, introducing users to the concept of the metaverse.<br/>
-
--Dynamic Content: Discover various metaverse worlds available for exploration, each accompanied by descriptive text and engaging visuals.<br/>
-
--Feedback Integration: Feedback and reviews by the users can be displayed to increase sense of trust and authenticity.`,
-        githubLink: "https://github.com/abhicode7/holovista-metaverse-website",
-        liveLink: "https://holovista.vercel.app/",
-        imageLink: "https://github.com/abhicode7/holovista-metaverse-website/assets/87370756/e3c58eb8-4a1b-487d-89e3-c31aa413ef8c"
-    },
-
     {
         id: 4,
         title: "Draggable Notes App",
@@ -58,6 +50,21 @@ screen size.<br/>
         githubLink: "https://github.com/abhicode7/draggable-notes-app",
         liveLink: "https://abhicode7.github.io/draggable-notes-app/",
         imageLink: "https://github.com/abhicode7/draggable-notes-app/assets/87370756/5de7ee61-5f76-4e08-9541-e61d4d2c9939"
+    },
+
+    {
+        id: 5,
+        title: "Holovista Metaverse Demo",
+        description: `Features 🛰️<br/>-Interactive Navigation: Seamlessly explore different sections of the website, including About, Explore, Insights, and more.<br/>
+
+-Immersive Hero Section: Engage with visually stunning animations and captivating visuals in the Hero section, introducing users to the concept of the metaverse.<br/>
+
+-Dynamic Content: Discover various metaverse worlds available for exploration, each accompanied by descriptive text and engaging visuals.<br/>
+
+-Feedback Integration: Feedback and reviews by the users can be displayed to increase sense of trust and authenticity.`,
+        githubLink: "https://github.com/abhicode7/holovista-metaverse-website",
+        liveLink: "https://holovista.vercel.app/",
+        imageLink: "https://github.com/abhicode7/holovista-metaverse-website/assets/87370756/e3c58eb8-4a1b-487d-89e3-c31aa413ef8c"
     }
     
 ];
